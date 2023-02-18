1942739ce3ab8b9c2022614a3ae99539eaa9ebeb
TusharCoder-Ing <75789015+TusharCoder-Ing@users.noreply.github.com>
2023-02-18 14:06:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNav(){
+    document.getElementById("navbar").style.width="30vw"
+    document.getElementById("hide-nav").style.fontSize="5vw"
+}
+function hideNav(){
+    document.getElementById("navbar").style.width="0vw"
+    document.getElementById("hide-nav").style.fontSize="0vw"
+
+}
